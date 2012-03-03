ec3680280b43037925e3a3316a77c9b5d662aab1
altnight <altnightsight@gmail.com>
2012-03-03 19:06:29 +0900

    setupとか追加とか

---
+++ apt-setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+sudo apt-get remove -y tracker;
+sudo apt-get remove -y thunderbird;
+sudo apt-get remove -y pidgin;
+sudo apt-get remove -y xchat;
+sudo apt-get remove -y listen;
+sudo apt-get remove -y gimp;
+sudo apt-get remove -y xsane;
+sudo apt-get remove -y abiword;
+sudo apt-get remove -y gnumeric;
+sudo apt-get remove -y catfish;
+sudo apt-get remove -y evolution;
+sudo apt-get remove -y stardict;
+sudo apt-get remove -y gnome-game;
+
+sudo apt-get update;
+sudo apt-get upgrade -y;
+sudo apt-get update;
+
+sudo apt-get install -y language-pack-ja;
+sudo dpkg-reconfigure locales;
+
+cat >> ~/.bash_profile <<EOF
+if [ -f ~/.bashrc ] ; then #.bashrcがあるならそっちをつかう
+     . ~/.bashrc
+fi
+EOF
+
+
+sudo apt-get install -y ntp;
+sudo apt-get install -y vim;
+sudo apt-get install -y git;
+sudo apt-get install -y tree;
+sudo apt-get install -y tig;
+sudo apt-get install -y htop;
+sudo apt-get install -y tmux;
+sudo apt-get install -y sqlite3;
+sudo apt-get install -y ctags;
+
+mkdir ~/.bundle;
+git clone git://github.com/Shougo/neobundlevim.git ~/.bundle/neobundle.vim;
+
+git config --global user.name "altnight";
+git config --global user.email altnightsight@gmail.com;
+git config --global core.editor 'vim -c "set fenc=utf-8"';
+
+sudo apt-get install -y python-setuptools;
+sudo easy_install pip;
+pip install ipython;
+mkdir ~/venvs;
+#pip install virtualenv;
+#pip install virtualenvwrapper;
+
+#sudo cat "tmpfs /tmp tmpfs defaults,noatime,size=378m 0 0" >> /etc/fstab
