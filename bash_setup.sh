b414b31222d0c7ba9c0a5f4f00db4343e6876bd8
altnight <altnightsight@gmail.com>
2012-07-31 18:57:52 +0900

    .dotはいらない気がした

--- bash_setup.sh
+++ bash_setup.sh
@@ -3,12 +3,12 @@
 DOT_FILES=( .vimrc .tmux.conf .bashrc .hgrc)
 
 for file in ${DOT_FILES[@]}
-do
-  if [ -a $HOME/$file ]; then
-    ln -s $HOME/dotfiles/$file $HOME/$file.dot
-    echo "ファイルが存在しますから.dotファイルつくるよ: $file"
-  else
-    ln -s $HOME/dotfiles/$file $HOME/$file
-    echo "シンボリックリンクを貼りました: $file"
-  fi
-done
+  do
+    #if [ -a $HOME/$file ]; then
+      #ln -s $HOME/dotfiles/$file $HOME/$file.dot
+      #echo "ファイルが存在しますから.dotファイルつくるよ: $file"
+    #else
+      ln -s $HOME/dotfiles/$file $HOME/$file
+      echo "シンボリックリンクを貼りました: $file"
+    #fi
+  done
