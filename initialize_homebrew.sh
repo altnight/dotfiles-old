4064c075fdb128032435dfe12af44337dba5ea09
altnight <altnightsight@gmail.com>
2013-02-20 12:43:17 +0900

    add homebrew initialize settings

---
+++ initialize_homebrew.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo initialize start
+echo brew update start
+brew update
+echo brew update end
+echo brew install start
+brew installack autoconf automake bash bash-completion ctags freetype2 gdbm git gnu-sed htop-osx libevent mercurial pcre readline rlwrap sqlite tig tmux tree vim wget zsh coreutils proctools
+echo brew install end
+echo brew doctor start
+brew doctor
+echo brew doctor end
+echo initialize end
