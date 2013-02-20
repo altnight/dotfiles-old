3ac7181beb4e2e86432ffbf87f7423b3c43a371d
altnight <altnightsight@gmail.com>
2013-02-20 13:54:35 +0900

    git push デフォルト時挙動指定 for 1.8 or later

--- apt-setup.sh
+++ apt-setup.sh
@@ -63,6 +63,7 @@ git config --global alias.st status
 git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.d diff
+git config --global push.default simple
 
 # Pythonまわりの設定
 sudo apt-get install -y python-pip;
