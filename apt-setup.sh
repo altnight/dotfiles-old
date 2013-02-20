88693c571597b446de49120c6381228e551e6dd7
altnight <altnightsight@gmail.com>
2013-02-20 11:28:41 +0900

    add tmux copy paste

--- apt-setup.sh
+++ apt-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#不必要っぽいのをとりあえず消す
+# 不必要っぽいのをとりあえず消す
 sudo apt-get remove -y tracker;
 sudo apt-get remove -y thunderbird;
 sudo apt-get remove -y pidgin;
@@ -15,12 +15,12 @@ sudo apt-get remove -y evolution;
 sudo apt-get remove -y stardict;
 sudo apt-get remove -y gnome-game;
 
-#とりあえずアップデート
+# とりあえずアップデート
 sudo apt-get update;
 sudo apt-get upgrade -y;
 sudo apt-get update;
 
-#いちおういれる
+# いちおういれる
 sudo apt-get install -y language-pack-ja;
 sudo dpkg-reconfigure locales;
 
@@ -40,14 +40,21 @@ sudo apt-get install -y htop;
 sudo apt-get install -y tmux;
 sudo apt-get install -y sqlite3;
 sudo apt-get install -y ctags;
+sudo apt-get install -y zsh;
 
-#NeoBundleするための準備
+# tmux pasteboard
+# git clone git://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard.git
+# cd ./tmux-MacOSX-pasteboard
+# make reattach-to-user-namespace
+# cp reattach-to-user-namespace /usr/local/bin
+
+# NeoBundleするための準備
 mkdir ~/.vim;
 sudo chown -R $USER:$USER.vim;
 sudo chown -R $USER:$USER.viminfo;
 git clone git://github.com/Shougo/neobundle.vim.git ~/.vim/neobundle.vim;
 
-#gitのデフォルト設定
+# gitのデフォルト設定
 git config --global user.name "altnight";
 git config --global user.email altnightsight@gmail.com;
 git config --global core.editor 'vim -c "set fenc=utf-8"';
@@ -57,10 +64,8 @@ git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.d diff
 
-#Pythonまわりの設定
+# Pythonまわりの設定
 sudo apt-get install -y python-pip;
 mkdir ~/venvs;
 sudo pip install virtualenv;
 sudo pip install virtualenvwrapper;
-
-#sudo cat "tmpfs /tmp tmpfs defaults,noatime,size=378m 0 0" >> /etc/fstab
